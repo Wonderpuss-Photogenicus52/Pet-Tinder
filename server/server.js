846134c41883095a92986af4abd1689c99206c18
jhpaige <jhpaige@usc.edu>
2022-08-13 11:40:12 -0700

    testing
    Co-authored-by: Vance M Mcgrady <VanceMcGrady@users.noreply.github.com>
    Co-authored-by: CodeMahmoud <CodeMahmoud@users.noreply.github.com>
    Co-authored-by: Michael sun <sunhonghaoparis@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const apiRouter = require("./router");
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
