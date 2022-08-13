de1136e61a554775add88fc12626869a6e58e353
Michael sun <106768935+sunhonghaoparis@users.noreply.github.com>
2022-08-12 19:04:23 -0700

    signup page done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as ReactDOMClient from "react-dom/client";
 import App from './App';
+import  '../src/components/tinder.png'
 
 ReactDOMClient.createRoot(document.querySelector("#root")).render(<App />);
