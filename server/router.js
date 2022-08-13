d2ef026a492accc319426251cebbec97f9bb1149
jhpaige <jhpaige@usc.edu>
2022-08-12 18:35:44 -0700

    Initializing server

---
+++ server/router.js
@@ -0,0 +1,26 @@
+const express = require("express");
+// placeholder for controller
+const controller = require("./conftrollerFolder");
+const router = express.Router();
+
+router.get('/login', );
+
+router.post('/signup', );
+
+router.get('/home', );
+
+// ADD CONTROLLER CONNECTIONS
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const cors = require("cors");
+
+const apiRouter = require("./server/router");
